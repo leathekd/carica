d89e1118369a8b4c052a2bcc25b59270e095ba0e
David Leatherman <git@david.leatherman.fm>
2019-07-22 17:59:11 -0400

    bump version and dependencies

--- project.clj
+++ project.clj
@@ -1,14 +1,9 @@
-(defproject sonian/carica "1.2.3-SNAPSHOT"
+(defproject carica "1.2.3"
   :description "A flexible configuration library"
-  :url "https://github.com/sonian/carica"
-  :dependencies [[cheshire "5.3.1"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [org.clojure/tools.reader "0.8.3"]]
+  :url "https://github.com/leathekd/carica"
+  :dependencies [[cheshire "5.8.1"]
+                 [org.clojure/tools.logging "0.4.1"]
+                 [org.clojure/tools.reader "1.3.2"]]
   :profiles {:dev
              {:resource-paths ["etc"]
-              :dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
-  :aliases {"all" ["with-profile" "dev:1.4,dev"]}
-  ;; For Lein 1
-  :dev-dependencies [[org.clojure/clojure "1.4.0"]]
-  :dev-resources-path "etc")
+              :dependencies [[org.clojure/clojure "1.10.1"]]}})
